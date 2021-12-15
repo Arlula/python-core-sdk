62b6b48674d9a50c31b9b5a16e303fea2ee498e6
Charlie Wilson <charlie@spiralblue.space>
2021-12-15 12:54:52 +1100

    Invert null check to actually make sense

--- arlulacore/archive.py
+++ arlulacore/archive.py
@@ -164,7 +164,7 @@ class SearchRequest(ArlulaObject):
             "west": self.west, "supplier": self.supplier, "off-nadir": self.off_nadir}
 
         query_params = {k: v for k, v in param_dict.items()
-            if v is not None or v == "None"}
+            if v is not None and v != "None"}
 
         return query_params
 
