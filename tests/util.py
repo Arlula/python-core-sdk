1c8452cab9584982ab50b426db80d5a09a0edd61
Harrison Steyn <steynharrison1@gmail.com>
2021-12-16 17:04:18 +1100

    Correctly pass url to session during creation

--- tests/util.py
+++ tests/util.py
@@ -3,4 +3,4 @@ import arlulacore
 import os
 
 def create_test_session() -> arlulacore.Session:
-    return arlulacore.Session(os.getenv("API_KEY"), os.getenv("API_SECRET"), os.getenv("API_HOST"))
+    return arlulacore.Session(os.getenv("API_KEY"), os.getenv("API_SECRET"), url=os.getenv("API_HOST"))
