d8cd665891d9990d916aeab8bb606c510dc07076
Pericles Telemachou <pericles@outlook.com.au>
2022-07-04 15:26:20 +1000

    Version bump for release 3.0.0, for api version 2022-07

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='arlulacore',
-    version='2.0.3',
+    version='3.0.0',
     author="Arlula",
     author_email="tech@arlula.com",
     description="A package to facilitate access to the Arlula Imagery Marketplace API",
