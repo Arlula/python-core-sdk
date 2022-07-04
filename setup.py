0a94d62370fe3fb61395a5a63eb02d36320b767f
Harrison Steyn <steynharrison1@gmail.com>
2022-07-05 06:45:02 +1000

    Bump Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='arlulacore',
-    version='3.0.0',
+    version='3.0.1',
     author="Arlula",
     author_email="tech@arlula.com",
     description="A package to facilitate access to the Arlula Imagery Marketplace API",
