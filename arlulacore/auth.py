c46298590ad545e560c8c2e323290715a231edb7
Harrison Steyn <steynharrison1@gmail.com>
2022-07-10 21:51:42 +1000

    Bump version

--- arlulacore/auth.py
+++ arlulacore/auth.py
@@ -10,7 +10,7 @@ from .exception import ArlulaSessionError
 name = "arlulacore"
 
 # User agent setting
-sdk_version = "3.0.1"
+sdk_version = "3.0.2"
 py_version = sys.version.split(' ')[0]
 os_version = platform.platform()
 def_ua = "core-sdk " + \
