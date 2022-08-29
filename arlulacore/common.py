b921beed7d7e722ea5eb04b64c78a68780446a9e
Harrison Steyn <steynharrison1@gmail.com>
2022-08-29 12:00:17 +1000

    Add format specifier to the root arlulaobject class

--- arlulacore/common.py
+++ arlulacore/common.py
@@ -1,3 +1,18 @@
-class ArlulaObject:
+import abc
+import json
+
+class ArlulaObject(abc.ABC):
     def __repr__(self):
         return str(['{}: {}'.format(attr, value) for attr, value in self.__dict__.items()])[1:-1].replace('\'', '')
+
+    @abc.abstractmethod
+    def dict(self):
+        pass
+
+    def format(self, format: str) -> str:
+        if format == "json":
+            return json.dumps(self.dict())
+        if format == "text":
+            return self
+        if format == "table":
+            return self
