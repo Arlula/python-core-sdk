0fcc6a4756c707f5aa264e80e470a3479209c3bf
Harrison Steyn <steynharrison1@gmail.com>
2023-09-06 10:59:56 +1000

    Add checks for new required environment variables

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -22,3 +22,15 @@ class TestEnv(unittest.TestCase):
         self.assertNotEqual(os.getenv("API_RESOURCE_ID"), None)
     def test_team_id(self):
         self.assertNotEqual(os.getenv("API_TEAM_ID"), None)
+    def test_collection_id(self):
+        self.assertNotEqual(os.getenv("API_COLLECTION_ID"), None)
+    def test_collection_item_id(self):
+        self.assertNotEqual(os.getenv("API_COLLECTION_ITEM_ID"), None)
+    def tasking_order_1(self):
+        self.assertNotEqual(os.getenv("API_TASKING_ORDERING_ID_1"), None)
+        self.assertNotEqual(os.getenv("API_TASKING_BUNDLE_KEY_1"), None)
+        self.assertNotEqual(os.getenv("API_TASKING_ORDER_LICENSE_HREF_1"), None)
+    def tasking_order_2(self):
+        self.assertNotEqual(os.getenv("API_TASKING_ORDERING_ID_2"), None)
+        self.assertNotEqual(os.getenv("API_TASKING_BUNDLE_KEY_2"), None)
+        self.assertNotEqual(os.getenv("API_TASKING_ORDER_LICENSE_HREF_2"), None)
