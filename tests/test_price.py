306c52410e39e0e2f5916e90a06dc7a993c82f1c
Harrison Steyn <harrison@arlula.com>
2024-08-14 17:09:05 +1000

    Add new tests for calculating tasking price

--- tests/test_price.py
+++ tests/test_price.py
@@ -1,11 +1,18 @@
 import unittest
 import arlulacore
 
-class TestSearchRequest(unittest.TestCase):
+class TestCalculatePrice(unittest.TestCase):
 
-    def test_calculate(self):
+    def test_calculate_archive(self):
         self.assertEqual(arlulacore.calculate_price(100, 0, 0), 100)
         self.assertEqual(arlulacore.calculate_price(100, 50, 0), 200)
         self.assertEqual(arlulacore.calculate_price(100, 50, 100), 300)
         self.assertEqual(arlulacore.calculate_price(100, 75, 124), 300)
         self.assertEqual(arlulacore.calculate_price(100, 75, 126), 400)
+    
+    def test_calculate_tasking(self):
+        self.assertEqual(arlulacore.calculate_price(100, 0, 0, 0, 0, 0, 0), 100)
+        self.assertEqual(arlulacore.calculate_price(100, 50, 0, 50, 0, 50, 0), 300)
+        self.assertEqual(arlulacore.calculate_price(100, 0, 1000, 0, 600, 0, 165), 1900)
+        self.assertEqual(arlulacore.calculate_price(100, 75, 124, 12, 322, 432, 321), 1400)
+        self.assertEqual(arlulacore.calculate_price(100, 25, 100, 400, 1600, 3200, 6400), 11900)
